7684512f1666b3b2b96e66bf44dd2c115c27a569
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-10-18 19:06:20 +0200

    Added ragdoll entity

--- src/main.cpp
+++ src/main.cpp
@@ -4,70 +4,213 @@
 #include <Box2D/Box2D.h>
 #include <string>
 #include <vector>
-#include "../3rdparty/picojson/picojson.h"
+#include <cmath>
 
-class Entity {
+
+class Box {
 protected:
-    b2BodyDef *boxDef;
-    b2PolygonShape *boxShape;
-    b2FixtureDef *boxFixDef;
+    b2Body* body;
+    b2PolygonShape* shape;
 
 public:
-    b2Body *box;
+    Box(b2World *world, const b2Vec2 &position, const b2Vec2 &size, 
+        const bool &is_dynamic) { 
 
-    Entity(b2World *world, const b2Vec2 &position, const b2Vec2 &size, 
-           const bool &is_dynamic) { 
+        b2BodyDef *boxDef = new b2BodyDef();
+        boxDef->position.Set(position.x, position.y);
+        boxDef->type = (is_dynamic) ? b2_dynamicBody : b2_staticBody;
 
-        this->boxDef = new b2BodyDef();
-        this->boxDef->position.Set(position.x, position.y);
-        this->boxDef->type = (is_dynamic) ? b2_dynamicBody : b2_staticBody;
+        this->body = world->CreateBody(boxDef);
 
-        this->box = world->CreateBody(boxDef);
+        this->shape = new b2PolygonShape();
+        this->shape->SetAsBox(size.x / 2.0f, size.y / 2.0f);
 
-        this->boxShape = new b2PolygonShape();
-        this->boxShape->SetAsBox(size.x, size.y);
+        b2FixtureDef *boxFixDef = new b2FixtureDef();
+        boxFixDef->shape = this->shape;
+        boxFixDef->density = 1.0f;
+        boxFixDef->friction = 0.3f;
 
-        this->boxFixDef = new b2FixtureDef();
-        this->boxFixDef->shape = boxShape;
-        this->boxFixDef->density = 1.0f;
-        this->boxFixDef->friction = 0.3f;
+        this->body->CreateFixture(boxFixDef);
+    }
 
-        this->box->CreateFixture(boxFixDef);
+    b2Body* getBody() {
+        return this->body;
+    }
+
+    b2PolygonShape* getShape() {
+        return this->shape;
     }
 
     std::vector<b2Vec2> getVertices() {
-        int count = this->boxShape->GetVertexCount();
+        int count = this->shape->GetVertexCount();
         std::vector<b2Vec2> verts;
 
         
         for (int i = 0; i < count; i++) {
-            b2Vec2 vert = this->boxShape->GetVertex(i);
-            verts.push_back(this->box->GetWorldPoint(vert));
+            b2Vec2 vert = this->shape->GetVertex(i);
+            verts.push_back(this->body->GetWorldPoint(vert));
         }
 
         return verts;
     }
 
     b2Vec2 getPosition() {
-        return this->box->GetPosition();
+        return this->body->GetPosition();
     }
 
     double getAngle() {
-        return this->box->GetAngle();
+        return this->body->GetAngle();
     }
     
-    void drawWireframe(sf::RenderWindow* window) {
+    void drawWireframe(sf::RenderWindow* window, const sf::Color& color) {
         std::vector<b2Vec2> verts = this->getVertices();
 
         sf::VertexArray lines(sf::LinesStrip, 5);
 
-        for (int i = 0; i < verts.size(); i++) {
-            lines[i].position = sf::Vector2f(verts[i].x, verts[i].y);
+        for (int i = 0; i < verts.size() + 1; i++) {
+            lines[i].position = sf::Vector2f(verts[i % verts.size()].x, verts[i % verts.size()].y);
+            lines[i].color = color;
         }
 
-        lines[4].position = sf::Vector2f(verts[0].x, verts[0].y);
-
         window->draw(lines);
+    }
+};
+
+class Entity {
+protected:
+    std::vector<Box*> boxes;
+    std::vector<b2RevoluteJoint*> joints;
+
+public:
+    void addBox(Box* box) {
+        this->boxes.push_back(box);
+    }
+
+    void addJoint(b2RevoluteJoint* joint) {
+        this->joints.push_back(joint);
+    }
+
+    std::vector<Box*> getBoxes() {
+        return this->boxes;
+    }
+
+    std::vector<b2RevoluteJoint*> getJoints() {
+        return this->joints;
+    }
+
+    void drawWireframe(sf::RenderWindow* window, const sf::Color& color) {
+        for (int i = 0; i < boxes.size(); i++) {
+            boxes[i]->drawWireframe(window, color);
+        }
+    }
+};
+
+class Ragdoll : public Entity {
+private:
+    void addLimbs(b2World* world, const b2Vec2 &position) {
+        b2RevoluteJointDef revoluteJointDef;
+        revoluteJointDef.collideConnected = false;
+        revoluteJointDef.enableLimit = true;
+
+        Box* torso = new Box(world, position + b2Vec2(0, -2), b2Vec2(22, 23), true);
+        Box* head = new Box(world, position + b2Vec2(0, -20), b2Vec2(12, 12), true);
+        revoluteJointDef.bodyA = torso->getBody();
+        revoluteJointDef.bodyB = head->getBody();
+        revoluteJointDef.localAnchorA.Set(0, -12);
+        revoluteJointDef.localAnchorB.Set(0, 6);
+        revoluteJointDef.lowerAngle = -15 * (M_PI / 180.0);
+        revoluteJointDef.upperAngle = 15 * (M_PI / 180.0);
+        b2RevoluteJoint* joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(torso);
+        this->addBox(head);
+        this->addJoint(joint);
+
+        Box* leftForearm = new Box(world, position + b2Vec2(-20, -9), b2Vec2(13, 7), true);
+        revoluteJointDef.bodyB = leftForearm->getBody();
+        revoluteJointDef.localAnchorA.Set(-11, -10);
+        revoluteJointDef.localAnchorB.Set(5, 0);
+        revoluteJointDef.lowerAngle = -90 * (M_PI / 180.0);
+        revoluteJointDef.upperAngle = 90 * (M_PI / 180.0);
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(leftForearm);
+        this->addJoint(joint);
+
+        Box* leftUpperLeg = new Box(world, position + b2Vec2(-3, 14), b2Vec2(8, 14), true);
+        revoluteJointDef.bodyB = leftUpperLeg->getBody();
+        revoluteJointDef.localAnchorA.Set(-3, 9);
+        revoluteJointDef.localAnchorB.Set(0, -7);
+        revoluteJointDef.lowerAngle = 0 * (M_PI / 180.0);
+        revoluteJointDef.upperAngle = 90 * (M_PI / 180.0);
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(leftUpperLeg);
+        this->addJoint(joint);
+
+        Box* rightForearm = new Box(world, position + b2Vec2(20, -9), b2Vec2(13, 7), true);
+        revoluteJointDef.bodyB = rightForearm->getBody();
+        revoluteJointDef.localAnchorA.Set(11, -10);
+        revoluteJointDef.localAnchorB.Set(-5, 0);
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(rightForearm);
+        this->addJoint(joint);
+
+        Box* rightUpperLeg = new Box(world, position + b2Vec2(3, 14), b2Vec2(8, 14), true);
+        revoluteJointDef.bodyB = rightUpperLeg->getBody();
+        revoluteJointDef.localAnchorA.Set(3, 9);
+        revoluteJointDef.localAnchorB.Set(0, -7);
+        revoluteJointDef.lowerAngle = -90 * (M_PI / 180.0);
+        revoluteJointDef.upperAngle = 0 * (M_PI / 180.0);
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(rightUpperLeg);
+        this->addJoint(joint);
+
+        Box* leftLowerLeg = new Box(world, position + b2Vec2(-3, 30), b2Vec2(8, 14), true);
+        revoluteJointDef.bodyA = leftUpperLeg->getBody();
+        revoluteJointDef.bodyB = leftLowerLeg->getBody();
+        revoluteJointDef.localAnchorA.Set(0, 7);
+        revoluteJointDef.localAnchorB.Set(0, -7);
+        revoluteJointDef.lowerAngle = -90 * (M_PI / 180.0);
+        revoluteJointDef.upperAngle = 0 * (M_PI / 180.0);
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(leftLowerLeg);
+        this->addJoint(joint);
+
+        Box* rightLowerLeg = new Box(world, position + b2Vec2(3, 30), b2Vec2(8, 14), true);
+        revoluteJointDef.bodyA = rightUpperLeg->getBody();
+        revoluteJointDef.bodyB = rightLowerLeg->getBody();
+        revoluteJointDef.localAnchorA.Set(0, 7);
+        revoluteJointDef.localAnchorB.Set(0, -7);
+        revoluteJointDef.lowerAngle = 0 * (M_PI / 180.0);
+        revoluteJointDef.upperAngle = 90 * (M_PI / 180.0);
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(rightLowerLeg);
+        this->addJoint(joint);
+
+        Box* leftArm = new Box(world, position + b2Vec2(-23, -9), b2Vec2(20, 7), true);
+        revoluteJointDef.bodyA = leftForearm->getBody();
+        revoluteJointDef.bodyB = leftArm->getBody();
+        revoluteJointDef.localAnchorA.Set(-6, 0);
+        revoluteJointDef.localAnchorB.Set(5, 0);
+        revoluteJointDef.lowerAngle = 0;
+        revoluteJointDef.upperAngle = 90 * (M_PI / 180.0);
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(leftArm);
+        this->addJoint(joint);
+
+        Box* rightArm = new Box(world, position + b2Vec2(23, -9), b2Vec2(20, 7), true);
+        revoluteJointDef.bodyA = rightForearm->getBody();
+        revoluteJointDef.bodyB = rightArm->getBody();
+        revoluteJointDef.localAnchorA.Set(6, 0);
+        revoluteJointDef.localAnchorB.Set(-5, 0);
+        revoluteJointDef.lowerAngle = -90 * (M_PI / 180.0);
+        revoluteJointDef.upperAngle = 0;
+        joint = (b2RevoluteJoint*) world->CreateJoint(&revoluteJointDef);
+        this->addBox(rightArm);
+        this->addJoint(joint);
+    }
+
+public:
+    Ragdoll(b2World* world, const b2Vec2 &position) {
+        addLimbs(world, position);
     }
 };
 
@@ -76,12 +219,15 @@ int main() {
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    b2World world(b2Vec2(0.0f, 1000.0f));
-    b2BodyDef groundDef;
-    groundDef.position.Set(200.0f, 200.0f);
+    b2World world(b2Vec2(0.0f, 500.0f));
 
-    Entity *box = new Entity(&world, b2Vec2(50.0, 50.0), b2Vec2(10.0, 10.0), true); 
-    Entity *box2 = new Entity(&world, b2Vec2(110.0, 100.0), b2Vec2(50.0, 10.0), false); 
+    std::vector<Ragdoll*> ragdolls;
+
+    Box *box2 = new Box(&world, b2Vec2(500.0, 600.0), b2Vec2(50.0, 10.0), false); 
+
+    Entity *entity2 = new Entity();
+    entity2->addBox(box2);
+
     double timeStep = 1.0 / 60.0;
 
     while (window.isOpen()) {
@@ -89,20 +235,27 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
+            } else if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    Ragdoll* ragdoll = new Ragdoll(&world, b2Vec2(mousePos.x, mousePos.y));
+                    ragdolls.push_back(ragdoll); 
+                }
             }
         }
 
         world.Step(timeStep, 8, 3);
 
-        b2Vec2 pos = box->getPosition();
-        double angle = box->getAngle();
-
         //std::cout << pos.x << ", " << pos.y << std::endl;
         //std::cout << angle << std::endl;
 
         window.clear();
-        box->drawWireframe(&window);
-        box2->drawWireframe(&window);
+    
+        for (auto r : ragdolls) {
+            r->drawWireframe(&window, sf::Color::Red);
+        }
+
+        entity2->drawWireframe(&window, sf::Color::Green);
         window.display();
 
         sf::sleep(sf::seconds(1.0/60.0));
